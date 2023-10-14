7ea8c96c1b636266bb1071e0165c9e01cc36868c
AmarnathCKR <ckramarnath@gmail.com>
2023-10-14 13:21:40 +0530

    Bugs fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,13 @@ import jsPDF from "jspdf";
 /* eslint-disable react/prop-types */
 function Home({ loading }) {
   const [graphData, setGraph] = useState();
-  const [portable, setPort] = useState();
+  const [hide, setHide] = useState(true);
   const printableRef = useRef(null);
 
   useEffect(() => {
     loading(true);
     fetchData().then(async (response) => {
-      loading(false);
+      
       const result = Object.keys(response.data.data[0])
         .filter((key) => key !== "data_year")
         .map((key) => ({
@@ -24,37 +24,46 @@ function Home({ loading }) {
             return acc;
           }, {}),
         }));
-        
+
       setGraph(result);
+      loading(false);
     });
   }, []);
 
   return (
     <>
       <div className="dark:bg-black bg-white h-screen flex justify-center items-center text-black dark:text-white">
-        <button className="flex border-4 rounded px-6 py-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
-            />
-          </svg>
+        
           <ReactToPrint
-            trigger={() => <button>Print this out!</button>}
+            trigger={() => (
+              <button className="flex border-4 rounded px-6 py-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6.72 13.829c-.24.03-.48.062-.72.096m.72-.096a42.415 42.415 0 0110.56 0m-10.56 0L6.34 18m10.94-4.171c.24.03.48.062.72.096m-.72-.096L17.66 18m0 0l.229 2.523a1.125 1.125 0 01-1.12 1.227H7.231c-.662 0-1.18-.568-1.12-1.227L6.34 18m11.318 0h1.091A2.25 2.25 0 0021 15.75V9.456c0-1.081-.768-2.015-1.837-2.175a48.055 48.055 0 00-1.913-.247M6.34 18H5.25A2.25 2.25 0 013 15.75V9.456c0-1.081.768-2.015 1.837-2.175a48.041 48.041 0 011.913-.247m10.5 0a48.536 48.536 0 00-10.5 0m10.5 0V3.375c0-.621-.504-1.125-1.125-1.125h-8.25c-.621 0-1.125.504-1.125 1.125v3.659M18 10.5h.008v.008H18V10.5zm-3 0h.008v.008H15V10.5z"
+                  />
+                </svg>
+                Print
+              </button>
+            )}
             content={() => printableRef.current}
-            
+            onBeforeGetContent={()=>setHide(false)}
+            onAfterPrint={()=>setHide(true)}
           />
-        </button>
+        
       </div>
-      <div style={{visibility : "hidden" , height : "0px"}} className="flex flex-col h-0 justify-center align-middle items-center">
+      <div
+        style={{ visibility: "hidden", height: "0px", display: hide?"none" : "block" }}
+        className="flex flex-col h-0 justify-center align-middle items-center"
+      >
         <div ref={printableRef}>
           {graphData &&
             graphData.map((d, index) => (
